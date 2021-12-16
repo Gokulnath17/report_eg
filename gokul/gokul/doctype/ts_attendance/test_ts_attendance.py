afc6a137c5919de23a7aa0086bc53824f1264831
workstation2.4 <workstation2_4@workstation2.4>
2021-12-16 13:08:14 +0530

    doctype created

---
+++ gokul/gokul/doctype/ts_attendance/test_ts_attendance.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, gokulnath and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class Testts_Attendance(unittest.TestCase):
+	pass
---
+++ gokul/gokul/doctype/ts_attendance/ts_attendance.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, gokulnath and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('ts_Attendance', {
+	// refresh: function(frm) {
+
+	// }
+});
